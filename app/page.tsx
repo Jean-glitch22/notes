c1d79a59748da7017bfb468e3d2ed387ecadf2fa
Jean Oberholzer <jeanoberholzer88@gmail.com>
2025-08-01 13:57:54 +0200

    V1

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { cookies } from "next/headers"
+import { createClient } from "@/lib/supabase/server"
+
+export default async function Home() {
+  const cookieStore = cookies ()
+  const supabase = await createClient (cookieStore)
+  
+  const data = await supabase.from("Notes").select()
+  console.log("Data: ", data)
+
+
+  return (
+    <div>
+      <h1>{data.data?.[0].content}</h1>
+    </div>
+  )
+}
